1bc1db37070da84e6ecc447d371eaa33ab7b7931
burbage stacey <rstacey06@gmail.com>
2019-10-25 18:38:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = {}
+  data.each do |key, value|
+    value.each do |value, names|
+      names.each do |name|
+        
+        if !hash[name]
+          hash[name] = {}
+        end
+
+        if !hash[name][key]
+          hash[name][key] = []
+        end
+
+        hash[name][key] << value.to_s
+
+      end
+    end
+  end
+  hash
 end
